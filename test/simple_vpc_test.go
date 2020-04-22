931fc2d7772c28bd5052d377d59197da08145117
Amanda Soares de Carvalho <amanda.carvalho@youse.com.br>
2020-04-22 19:12:39 -0300

    create test to simple-vpc

--- test/simple_vpc_test.go
+++ test/simple_vpc_test.go
@@ -1,8 +1,12 @@
 package test
 
 import (
+	"fmt"
+	"strings"
 	"testing"
 
+	"github.com/gruntwork-io/terratest/modules/aws"
+	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
 )
@@ -12,12 +16,18 @@ func TestSimpleVpc(t *testing.T) {
 
 	// Expected values
 	expVpcCidr := "10.120.0.0/16"
+	expectedName := fmt.Sprintf("vpc-terratest-%s", strings.ToLower(random.UniqueId()))
+	awsRegion := aws.GetRandomStableRegion(t, nil, nil)
 
 	terraformOptions := &terraform.Options{
 		TerraformDir: "../examples/simple-vpc",
 		Upgrade:      true,
-	}
 
+		Vars: map[string]interface{}{
+			"vpc_name": expectedName,
+			"region":   awsRegion,
+		},
+	}
 	// At the end of the test, run `terraform destroy` to clean up any resources that were created
 	// defer terraform.Destroy(t, terraformOptions)
 
